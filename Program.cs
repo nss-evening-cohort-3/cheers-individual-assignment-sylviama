fd63848ed7a1c2705c44b7a560d5e37f6caa65af
Sylvia Ma <xiangwma@umich.edu>
2016-07-11 20:48:35 -0500

    an a

--- Program.cs
+++ Program.cs
@@ -12,9 +12,18 @@ namespace Cheers
             Console.WriteLine("What's your name?");
             Console.Write(">");
             string name;
+            string require_an="aeiolnfshrmx";
             name=Console.ReadLine();
-            foreach(char element in name)
-                Console.WriteLine("give me a "+ element);
+            string a_name="a";
+            for(int i=0;i<name.Length;i++){
+                for(int j=0;j<require_an.Length;i++){
+            // foreach(char element in name){
+                    if(name[i]== require_an[j]){
+                        a_name= "an";
+                    }
+                }
+                Console.WriteLine("give me " + a_name +" "+ name[i]);
+            }
             Console.WriteLine(name.ToUpper() + " is granted.");
         }
     }
