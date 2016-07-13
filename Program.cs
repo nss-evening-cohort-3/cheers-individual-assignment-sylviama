676a3210e7bd3fcb77f41fe2df67a8c9b5d105b7
Sylvia Ma <xiangwma@umich.edu>
2016-07-13 13:30:48 -0500

    format validation for birthday

--- Program.cs
+++ Program.cs
@@ -16,29 +16,47 @@ namespace Cheers
             name=Console.ReadLine().ToLower();
 
             //birthday question
-            Console.WriteLine("What's your birthday?");
-            Console.Write(">");
-            string the_birthday;
-            the_birthday=Console.ReadLine();
+            bool keepGoing=true;
+            while(keepGoing){
+                Console.WriteLine("What's your birthday?");
+                Console.Write(">");
+                string the_birthday;
+                the_birthday=Console.ReadLine();
 
-            //name output
-            string require_an="aeiolnfshrmx";
-            string a_name="a";
-            for(int i=0;i<name.Length;i++){
-                if(require_an.IndexOf(name[i])>-1){
-                    a_name= "an";
-                }else{
-                    a_name= "a";
+                try{
+                //birthday calculation 
+                DateTime con_birthday=Convert.ToDateTime(the_birthday);
+                DateTime thisDay = DateTime.Today;
+                TimeSpan t=con_birthday-thisDay;
+
+                //name output
+                string require_an="aeiolnfshrmx";
+                string a_name="a";
+                for(int i=0;i<name.Length;i++){
+                    if(require_an.IndexOf(name[i])>-1){
+                        a_name= "an";
+                    }else{
+                        a_name= "a";
+                    }
+                    Console.WriteLine("give me " + a_name +" "+ name[i]);
                 }
-                Console.WriteLine("give me " + a_name +" "+ name[i]);
-            }
-            Console.WriteLine(name.ToUpper() + " is granted.");
+                Console.WriteLine(name.ToUpper() + " is granted.");
 
-            //birthday output
-            DateTime thisDay = DateTime.Today;
-            DateTime con_birthday=Convert.ToDateTime(the_birthday);
-            TimeSpan t=con_birthday-thisDay;
-            Console.WriteLine("Your birthday is "+ t.Days+ " day(s) away!");
+                //birthday output
+                Console.WriteLine("Your birthday is "+ t.Days+ " day(s) away!");
+                keepGoing=false;
+                
+                } catch(FormatException){
+                    Console.Write("Birthday input is not valid. ");
+                    continue;
+                }
+            }
+
+            
+
+            
+
+            
         }
     }
 }
